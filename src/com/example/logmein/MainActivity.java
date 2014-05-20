1673bbf0cb70247ec5405dae9afb0c36e53ab0fc
Shubham Chaudhary <me@shubhamchaudhary.in>
2014-05-20 21:23:28 +0530

    Network on seperate thread, onClick events listeners
    * Run all network operations on a sepearate thread
    * onClick events listeners created

--- src/com/example/logmein/MainActivity.java
+++ src/com/example/logmein/MainActivity.java
@@ -16,29 +16,66 @@ import android.view.View.OnClickListener;
 import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import java.net.*;
 import java.io.*;
 
 public class MainActivity extends ActionBarActivity implements OnClickListener{
-	//UI
+	///Class Variables
 	EditText textbox_username, textbox_password;
-	Button button_save, button_login;
-	//Database
+	Button button_save, button_login, button_logout;
+	TextView debugTextView;
 	SQLiteOpenHelper DBHELPER;
-	 SQLiteDatabase database;
-	 
+	SQLiteDatabase database;
+
+	/*
+	//Bypass android.os.NetworkOnMainThreadException
+	StrictMode.ThreadPolicy policy = new StrictMode.ThreadPolicy.Builder().permitAll().build();
+
+	StrictMode.setThreadPolicy(policy); 
+	*/
+	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
        
+		button_save=(Button)findViewById(R.id.button_save);
+		
+		button_login=(Button)findViewById(R.id.button_login);
+		button_login.setOnClickListener(new View.OnClickListener() {
+	         public void onClick(View v) {
+	             // Perform action on click
+	        	 Log.d("login","Insiide Login");
+	        	 System.out.println("Insinde login");
+	        	 try{
+	        		 login(textbox_username.getText().toString(),textbox_password.getText().toString());
+	        	 }catch(Exception e){
+	        		 //TODO
+	        		 System.out.println("Exception message: "+e.toString());
+	 			 }
+	         }
+	     });
+		
+		button_logout=(Button)findViewById(R.id.button_logout);
+		button_logout.setOnClickListener(new View.OnClickListener() {
+	         public void onClick(View v) {
+	             // Perform action on click
+	        	 Log.d("logout","Insiede Logout");
+	        	 System.out.println("Insiade logout");
+	        	 try{
+	        		 logout();
+	        	 }catch(Exception e){
+	        		 //TODO
+	        		 System.out.println("Exception message: "+e.toString());
+	 			 }
+	         }
+	     });
+
         textbox_username=(EditText)findViewById(R.id.edit_username);
         textbox_password=(EditText)findViewById(R.id.edit_password);
-		button_save=(Button)findViewById(R.id.button_save);
-		button_login=(Button)findViewById(R.id.button_login);
-		
 		if (savedInstanceState == null) {
 			getSupportFragmentManager().beginTransaction()
 					.add(R.id.container, new PlaceholderFragment()).commit();
@@ -90,6 +127,7 @@ public class MainActivity extends ActionBarActivity implements OnClickListener{
 		String b=textbox_password.getText().toString();
 		try{
 			//make a db connect to it add values to it (next task)	
+				//WTH
 				System.out.print("breakpoint1");
 				DBHELPER=new dbhelper(this);
 				System.out.print("breakpoint 2");
@@ -98,64 +136,90 @@ public class MainActivity extends ActionBarActivity implements OnClickListener{
 				button_save.setOnClickListener(this);
 				if(a!=null && b!=null)
 				{
-					
 					values.put(dbhelper.USERNAME,a);
 					values.put(dbhelper.PASSWORD,b);
-					
-					//values.put(dbhelper.PROFIT, c);
+
 					database.insert(dbhelper.TABLE,null, values);  
-		            Log.d("databaseconnected and values inserted", "withprimarykey kuch bhi par sahi hogi");
+					//Debug message
+		            Log.d("tag: main, onClick, try", "database connected and values inserted with primary key");	//Fuck you Vivek
 		            Toast.makeText(getApplicationContext(), a+" entered into your inventory", Toast.LENGTH_SHORT).show();
 				}
 				textbox_username.clearComposingText();
 				textbox_password.clearComposingText();
-				
+
 			}catch(Exception e){
 				System.out.println("ud gaya");
 			}
 
 			database.close();
 	}
-	
-    public static void login(String username, String password) throws Exception{
+
+    public void login(final String username, final String password) throws Exception {
+    	Thread thread = new Thread(new Runnable(){
+    		@Override
+    		public void run() {
+    			try {
+    				login_runner(username,password);    				
+    			} catch (Exception e) {
+    				e.printStackTrace();
+    			}
+    		}
+    	});
+    	thread.start(); 
+    }
+    
+    public void login_runner(String username, String password) throws Exception{
         System.out.println("Loggin in");
         //String username = "11uit424", password = "screwYou";
         String urlParameters = "user="+username+"&password="+password; // "param1=a&param2=b&param3=c";
 
         String request = "http://172.16.4.201/cgi-bin/login";
-        URL url = new URL(request);
+        URL puServerUrl = new URL(request);
 
-        URLConnection conn = url.openConnection();
-
-        conn.setDoOutput(true);
-
-        OutputStreamWriter writer = new OutputStreamWriter(conn.getOutputStream());
-
+        URLConnection puServerConnection = puServerUrl.openConnection();
+        puServerConnection.setDoOutput(true);
+        OutputStreamWriter writer = new OutputStreamWriter(puServerConnection.getOutputStream());
         writer.write(urlParameters);
         writer.flush();
-
+        
+        //Output
         String line;
-        BufferedReader reader = new BufferedReader(new InputStreamReader(conn.getInputStream()));
+        BufferedReader reader = new BufferedReader(new InputStreamReader(puServerConnection.getInputStream()));
 
         while ((line = reader.readLine()) != null) {
-            System.out.println(line);
+        	Log.w("html", line);
         }
         writer.close();
         reader.close();
     }
 
-    public static void logout() throws Exception {
+    public void logout() throws Exception {
+    	Thread thread = new Thread(new Runnable(){
+    		@Override
+    		public void run() {
+    			try {
+    				logout_runner();    				
+    			} catch (Exception e) {
+    				e.printStackTrace();
+    			}
+    		}
+    	});
+    	thread.start(); 
+    }
+    
+    public void logout_runner() throws Exception {
         System.out.println("Loggin out");
-        URL yahoo = new URL("http://172.16.4.201/cgi-bin/login?cmd=logout");
-        URLConnection yc = yahoo.openConnection();
-        BufferedReader in = new BufferedReader(
-                                new InputStreamReader(
-                                yc.getInputStream()));
+        URL puServerUrl = new URL("http://172.16.4.201/cgi-bin/login?cmd=logout");
+        URLConnection puServerConnection = puServerUrl.openConnection();
+        
+        //Get inputStream and show output
+        BufferedReader htmlBuffer = new BufferedReader(new InputStreamReader(puServerConnection.getInputStream()));
+    	//TODO parse output
         String inputLine;
-
-        while ((inputLine = in.readLine()) != null)
-            System.out.println(inputLine);
-        in.close();
+        while ((inputLine = htmlBuffer.readLine()) != null){
+        	Log.w("html", inputLine);
+        }
+        htmlBuffer.close();
     }
 
 
