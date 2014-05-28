ec232205251bdaf4f85bbf58fb19f57f86c0f9ae
Shubham Chaudhary <me@shubhamchaudhary.in>
2014-05-28 06:27:57 +0530

    Inserted file headers
    Even though Android is Apache 2.0 but developers may still release any
    application on android with a GNU License. Version 3 looks like the best
    option.

--- src/in/shubhamchaudhary/logmein/MainActivity.java
+++ src/in/shubhamchaudhary/logmein/MainActivity.java
@@ -1,3 +1,23 @@
+/**
+ *   LogMeIn - Automatically log into Panjab University Wifi Network
+ *
+ *   Copyright (c) 2014 Shubham Chaudhary <me@shubhamchaudhary.in>
+ *   Copyright (c) 2014 Vivek Aggarwal <vivekaggarwal92@gmail.com>
+ *
+ *   LogMeIn is free software: you can redistribute it and/or modify
+ *   it under the terms of the GNU General Public License as published by
+ *   the Free Software Foundation, either version 3 of the License, or
+ *   (at your option) any later version.
+ *
+ *   LogMeIn is distributed in the hope that it will be useful,
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *   GNU General Public License for more details.
+ *
+ *   You should have received a copy of the GNU General Public License
+ *   along with LogMeIn.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
 package in.shubhamchaudhary.logmein;
 
 import android.os.Bundle;
