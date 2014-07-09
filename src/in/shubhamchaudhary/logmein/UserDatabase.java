c8f3f239115e72f2e783852db987a443a88578c3
Shubham Chaudhary <me@shubhamchaudhary.in>
2014-07-10 02:47:33 +0530

    Added license and reorganised imports

--- src/in/shubhamchaudhary/logmein/UserDatabase.java
+++ src/in/shubhamchaudhary/logmein/UserDatabase.java
@@ -1,10 +1,31 @@
+/**
+ *   LogMeIn - Automatically log into Panjab University Wifi Network
+ *
+ *   Copyright (c) 2014 Tanjot Kaur <tanjot28@gmail.com>
+ *   Copyright (c) 2014 Shubham Chaudhary <me@shubhamchaudhary.in>
+ *
+ *   This file is part of LogMeIn.
+ *
+ *   LogMeIn is free software: you can redistribute it and/or modify
+ *   it under the terms of the GNU General Public License as published by
+ *   the Free Software Foundation, either version 3 of the License, or
+ *   (at your option) any later version.
+ *
+ *   LogMeIn is distributed in the hope that it will be useful,
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *   GNU General Public License for more details.
+ *
+ *   You should have received a copy of the GNU General Public License
+ *   along with LogMeIn.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
 package in.shubhamchaudhary.logmein;
 
-import android.support.v7.app.ActionBarActivity;
-import android.support.v7.app.ActionBar;
-import android.support.v4.app.Fragment;
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.support.v7.app.ActionBarActivity;
 import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
@@ -12,7 +33,6 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
 import android.widget.Spinner;
-import android.os.Build;
 
 public class UserDatabase extends ActionBarActivity {
 
