29b86b392104fb987714d8f1b2495aa3cf6ef4b4
Shubham Chaudhary <me@shubhamchaudhary.in>
2014-07-29 03:01:21 +0530

    Now shows password if checkbox ticked

--- src/in/shubhamchaudhary/logmein/ui/FragmentEdit.java
+++ src/in/shubhamchaudhary/logmein/ui/FragmentEdit.java
@@ -5,18 +5,23 @@ import in.shubhamchaudhary.logmein.R.id;
 import in.shubhamchaudhary.logmein.R.layout;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.text.method.HideReturnsTransformationMethod;
+import android.text.method.PasswordTransformationMethod;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.CheckBox;
+import android.widget.CompoundButton;
 import android.widget.EditText;
 import android.widget.Spinner;
 
 public class FragmentEdit extends Fragment {
 
 	EditText username,password;
-	@Override
+	CheckBox cb_show_password;
 
+	@Override
 	public View onCreateView(LayoutInflater inflater,
 			ViewGroup container, Bundle savedInstanceState) {
 
@@ -27,8 +32,24 @@ public class FragmentEdit extends Fragment {
 				R.layout.fragment_edit_layout, container, false);
 		username = (EditText)v.findViewById(R.id.edit_username);
 		password = (EditText)v.findViewById(R.id.edit_password);
+		cb_show_password = (CheckBox) v.findViewById(R.id.cb_show_password);
 		username.setText(user.getUsername());
 		password.setText(user.getPassword());
+
+		// when user clicks on this checkbox, this is the handler.
+		cb_show_password.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+
+			public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+				// checkbox status is changed from uncheck to checked.
+				if (!isChecked) {
+					// show password
+					password.setTransformationMethod(PasswordTransformationMethod.getInstance());
+				} else {
+					// hide password
+					password.setTransformationMethod(HideReturnsTransformationMethod.getInstance());
+				}
+			}
+		});
 
 		return v;
 	}
