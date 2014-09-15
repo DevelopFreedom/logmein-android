fba9a42c105ed9a7523c0e874647fdb0ff004c52
Tanjot Kaur <tanjot28@gmail.com>
2014-09-16 00:19:22 +0530

    Update button is functional
    	Update button in ManageUser calls EditFragment

--- app/src/main/java/in/shubhamchaudhary/logmein/ui/ManageUser.java
+++ app/src/main/java/in/shubhamchaudhary/logmein/ui/ManageUser.java
@@ -1,17 +1,31 @@
 package in.shubhamchaudhary.logmein.ui;
 
+import android.content.Intent;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+
 import in.shubhamchaudhary.logmein.R;
 
 public class ManageUser extends ActionBarActivity {
 
+    Button update;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_manage_user);
+
+        update = (Button) findViewById(R.id.button_update);
+        update.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                update_user();
+            }
+        });
+
     }
 
 
@@ -32,5 +46,11 @@ public class ManageUser extends ActionBarActivity {
             return true;
         }
         return super.onOptionsItemSelected(item);
+    }
+
+    public void update_user(){
+        Intent intent = new Intent(this, UserDatabase.class);
+        startActivity(intent);
+
     }
 }
