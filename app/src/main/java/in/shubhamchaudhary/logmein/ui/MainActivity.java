80e09286faf698e536800df68e099db641420e8d
Shubham Chaudhary <me@shubhamchaudhary.in>
2014-10-05 03:40:29 +0530

    Optimized imports

--- app/src/main/java/in/shubhamchaudhary/logmein/ui/MainActivity.java
+++ app/src/main/java/in/shubhamchaudhary/logmein/ui/MainActivity.java
@@ -23,7 +23,6 @@ package in.shubhamchaudhary.logmein.ui;
 
 import android.app.AlertDialog;
 import android.app.Dialog;
-import android.content.Context;
 import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.SharedPreferences;
